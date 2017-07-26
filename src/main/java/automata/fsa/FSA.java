37384e352bcba6bfdcefbf7d6cb8d241743478d2
Hung-Wei Hsu <hsuhw@livemail.tw>
2017-07-27 06:10:01 +0800

    Update the practice of doing interface extensions

--- src/main/java/automata/fsa/FSA.java
+++ src/main/java/automata/fsa/FSA.java
@@ -1,7 +1,30 @@
 package automata.fsa;
 
 import automata.Automaton;
+import automata.part.State;
+import org.eclipse.collections.api.list.ImmutableList;
+import org.eclipse.collections.api.list.primitive.ImmutableBooleanList;
 
 public interface FSA extends Automaton
 {
+    @Override
+    boolean isDeterministic();
+
+    @Override
+    boolean isNondeterministic();
+
+    @Override
+    ImmutableList<State> getStates();
+
+    @Override
+    ImmutableBooleanList getStartStateTable();
+
+    @Override
+    boolean isStartState(int stateIndex);
+
+    @Override
+    ImmutableBooleanList getAcceptStateTable();
+
+    @Override
+    boolean isAcceptState(int stateIndex);
 }
--- src/main/java/automata/part/MutableState.java
+++ src/main/java/automata/part/MutableState.java
@@ -6,7 +6,28 @@ import java.util.List;
 
 public interface MutableState extends State
 {
+    @Override
+    boolean isDeterministic();
+
+    @Override
+    boolean isNondeterministic();
+
+    @Override
+    int getId();
+
+    @Override
+    String getDisplayName();
+
+    @Override
+    int getReferenceIndex();
+
+    @Override
+    ImmutableList<? extends Transition<? extends Label>> getDTransitions();
+
     void setDTransitions(ImmutableList<? extends Transition<? extends Label>> dTransitions);
+
+    @Override
+    ImmutableList<? extends ImmutableList<? extends Transition<? extends Label>>> getNTransitions();
 
     void setNTransitions(List<? extends ImmutableList<? extends Transition<? extends Label>>> nTransitions);
 }
