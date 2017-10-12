ee6b59b9af8987835a5a4ea03aaa36140fb2e413
Hung-Wei Hsu <hsuhw@livemail.tw>
2017-10-12 23:53:48 +0800

    Fix a missing static import

--- src/main/java/api/automata/AlphabetEncoderProvider.java
+++ src/main/java/api/automata/AlphabetEncoderProvider.java
@@ -2,9 +2,11 @@ package api.automata;
 
 import org.eclipse.collections.api.bimap.MutableBiMap;
 
+import static api.automata.AlphabetEncoder.Builder;
+
 public interface AlphabetEncoderProvider
 {
-    <S, T> AlphabetEncoder.Builder<S, T> builder(int sizeEstimate);
+    <S, T> Builder<S, T> builder(int sizeEstimate);
 
     <S, T> AlphabetEncoder<S, T> create(MutableBiMap<S, T> definition, S originEpsilon);
 }
