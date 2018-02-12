07d5f6184f96f04cd616f830fe398e7a5f8ed230
Hung-Wei Hsu <hsuhw@livemail.tw>
2018-02-12 19:49:53 +0800

    Add code for timing current thread CPU times

--- src/main/java/common/util/Stopwatch.java
+++ src/main/java/common/util/Stopwatch.java
@@ -1,8 +1,43 @@
 package common.util;
 
+import java.lang.management.ManagementFactory;
+import java.lang.management.ThreadMXBean;
+import java.time.Instant;
+
 public final class Stopwatch
 {
     private Stopwatch()
     {
+    }
+
+    public static long epochTimeInNs()
+    {
+        return System.currentTimeMillis() * 1000L;
+    }
+
+    public static long epochTimeInMs()
+    {
+        return System.currentTimeMillis();
+    }
+
+    public static long epochTimeInSec()
+    {
+        return Instant.now().getEpochSecond();
+    }
+
+    public static long currentThreadCpuTimeInNs()
+    {
+        ThreadMXBean bean = ManagementFactory.getThreadMXBean();
+        return bean.isCurrentThreadCpuTimeSupported() ? bean.getCurrentThreadCpuTime() : 0L;
+    }
+
+    public static long currentThreadCpuTimeInMs()
+    {
+        return currentThreadCpuTimeInNs() / 1000L;
+    }
+
+    public static long currentThreadCpuTimeInSec()
+    {
+        return currentThreadCpuTimeInNs() / 1000000L;
     }
 }
