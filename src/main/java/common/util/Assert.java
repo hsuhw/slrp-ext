b5a3396033eb563eea70766a998ffbe6ecdd8642
Hung-Wei Hsu <hsuhw@livemail.tw>
2018-02-24 22:34:26 +0800

    Change, as preferred, some IAEs and ISEs to NPEs

--- src/main/java/common/util/Assert.java
+++ src/main/java/common/util/Assert.java
@@ -9,70 +9,70 @@ public final class Assert
     public static void argumentNotNull(Object one)
     {
         if (one == null) {
-            throw new IllegalArgumentException("null argument given");
+            throw new NullPointerException("null argument given");
         }
     }
 
     public static void argumentNotNull(Object one, Object two)
     {
         if (one == null || two == null) {
-            throw new IllegalArgumentException("null argument given");
+            throw new NullPointerException("null argument given");
         }
     }
 
     public static void argumentNotNull(Object one, Object two, Object three)
     {
         if (one == null || two == null || three == null) {
-            throw new IllegalArgumentException("null argument given");
+            throw new NullPointerException("null argument given");
         }
     }
 
     public static void argumentNotNull(Object one, Object two, Object three, Object four)
     {
         if (one == null || two == null || three == null || four == null) {
-            throw new IllegalArgumentException("null argument given");
+            throw new NullPointerException("null argument given");
         }
     }
 
     public static void argumentNotNull(Object one, Object two, Object three, Object four, Object five)
     {
         if (one == null || two == null || three == null || four == null || five == null) {
-            throw new IllegalArgumentException("null argument given");
+            throw new NullPointerException("null argument given");
         }
     }
 
     public static void referenceNotNull(Object one)
     {
         if (one == null) {
-            throw new IllegalStateException("null reference occurred");
+            throw new NullPointerException("null reference occurred");
         }
     }
 
     public static void referenceNotNull(Object one, Object two)
     {
         if (one == null || two == null) {
-            throw new IllegalStateException("null reference occurred");
+            throw new NullPointerException("null reference occurred");
         }
     }
 
     public static void referenceNotNull(Object one, Object two, Object three)
     {
         if (one == null || two == null || three == null) {
-            throw new IllegalStateException("null reference occurred");
+            throw new NullPointerException("null reference occurred");
         }
     }
 
     public static void referenceNotNull(Object one, Object two, Object three, Object four)
     {
         if (one == null || two == null || three == null || four == null) {
-            throw new IllegalStateException("null reference occurred");
+            throw new NullPointerException("null reference occurred");
         }
     }
 
     public static void referenceNotNull(Object one, Object two, Object three, Object four, Object five)
     {
         if (one == null || two == null || three == null || four == null || five == null) {
-            throw new IllegalStateException("null reference occurred");
+            throw new NullPointerException("null reference occurred");
         }
     }
 }
