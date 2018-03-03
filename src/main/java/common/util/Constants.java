2d7034a473988b1d1500a83ff0c2534d62bd8eb0
Hung-Wei Hsu <hsuhw@livemail.tw>
2018-03-04 02:34:36 +0800

    Update `common.util.Constants`

--- src/main/java/common/util/Constants.java
+++ src/main/java/common/util/Constants.java
@@ -3,7 +3,7 @@ package common.util;
 public final class Constants
 {
     public static final String NOT_IMPLEMENTED_YET = "not implemented yet";
-    public static final String NO_MATCHED_IMPLEMENTATION_FOUND = "no matched implementation found";
+    public static final String NO_IMPLEMENTATION_FOUND = "no matched implementation found";
     public static final String DISPLAY_NEWLINE = System.getProperty("line.separator");
     public static final String DISPLAY_INDENT = "  ";
 
