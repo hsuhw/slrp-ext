14f7c0135b5d5fa1fd45b72593457ece9734315e
Hung-Wei Hsu <hsuhw@livemail.tw>
2018-03-13 14:50:25 +0800

    Remove trimmed and unused code

--- src/main/java/common/util/Constants.java
+++ src/main/java/common/util/Constants.java
@@ -7,11 +7,6 @@ public final class Constants
     public static final String DISPLAY_NEWLINE = System.getProperty("line.separator");
     public static final String DISPLAY_INDENT = "  ";
 
-    public enum Direction
-    {
-        FORWARD, BACKWARD
-    }
-
     private Constants()
     {
     }
